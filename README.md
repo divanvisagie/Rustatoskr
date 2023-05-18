25c7df11410cf47152d4feced9329079e53e7a88
Divan Visagie <me@divanv.com>
2023-05-18 08:52:44 +0200

    Simple bot stuff

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Rustatoskr
 Life's too short to use Go interfaces
+
+![Rustatoskr](docs/logo.jpg)
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use teloxide::prelude::*;
+use teloxide::{dispatching::dialogue::GetChatId, prelude::*, types::ChatAction};
 
 #[tokio::main]
 async fn main() {
@@ -8,7 +8,28 @@ async fn main() {
     let bot = Bot::from_env();
 
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        bot.send_dice(msg.chat.id).await?;
+        bot.send_chat_action(msg.chat.id, ChatAction::Typing)
+            .await?;
+
+        if let Some(text) = msg.text() {
+            if text == "/dice" {
+                bot.send_dice(msg.chat.id).await?;
+            } else {
+                bot.send_message(
+                    msg.chat.id,
+                    r#"Hi, I'm Rustatoskr, a bot designed because my creator 
+                    was annoyed with Go interfaces which are objectively bad.
+                    You can use /dice to throw a dice, or /help to get this message again."
+                    "#
+                    .trim_start_matches(" "),
+                )
+                .await?;
+            }
+        } else {
+            bot.send_message(msg.chat.id, "I can't do that dave")
+                .await?;
+        }
+
         Ok(())
     })
     .await;
