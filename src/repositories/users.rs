ff257f21f8dde63410d53315e9bd2afdf600c27b
Divan Visagie <visagiedivan@gmail.com>
2024-02-27 20:06:54 +0100

    Better user management

--- src/repositories/users.rs
+++ src/repositories/users.rs
@@ -1,5 +1,6 @@
 use async_trait::async_trait;
 use std::env;
+use tracing::warn;
 
 #[async_trait]
 pub trait UserRepository: Send + Sync {
@@ -18,7 +19,19 @@ impl FsUserRepository {
 impl UserRepository for FsUserRepository {
     async fn get_usernames(&mut self) -> Vec<String> {
         // get admin user from env
-        let admin_user = env::var("TELEGRAM_ADMIN").expect("TELEGRAM_ADMIN must be set");
-        return vec![admin_user];
+        let admin_user = env::var("TELEGRAM_ADMIN").unwrap_or_else(|_| {
+            panic!("TELEGRAM_ADMIN must be set");
+        });
+        let telegram_users = env::var("TELEGRAM_USERS").unwrap_or_else(|_| {
+            warn!("TELEGRAM_USERS not set, using only admin user");
+            "".to_string()
+        });
+        // split the string into a vector of strings
+        let telegram_users: Vec<String> =
+            telegram_users.split(",").map(|s| s.to_string()).collect();
+
+        let mut usernames = vec![admin_user];
+        usernames.extend(telegram_users);
+        usernames
     }
 }
